ead797fca59bd357677c76ef411d219144602dcd
meet1608 <meetvisodiya3@gmail.com>
2025-03-20 11:38:03 +0530

    why us page

---
+++ tankar/src/pages/Why_us.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const Why_us = () => {
+  return (
+    <div>
+        <Navbar/>
+      <h1>Why us</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Why_us;
