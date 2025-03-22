2e38f4140d7f017ee885727c316b3c316701cc5f
Hetuk <hetuk2212@gmail.com>
2025-03-22 12:53:30 +0530

    services

---
+++ tankar/src/constant/services.js
@@ -0,0 +1,51 @@
+import Service1 from '../assets/img/Services/service1.png';
+import Service2 from '../assets/img/Services/service2.png';
+import Service3 from '../assets/img/Services/service3.png';
+import Service4 from '../assets/img/Services/service4.png';
+import Service5 from '../assets/img/Services/service5.png';
+import Service6 from '../assets/img/Services/service6.png';
+
+export const services = [
+  {
+    id: 1,
+    title: "Software Development",
+    description: "We build robust, scalable, and high-performance software tailored to your business needs, leveraging the latest technologies for seamless operations.",
+    icon: Service1, 
+    delay: "200ms",
+  },
+  {
+    id: 2,
+    title: "IT Consulting & Advisory",
+    description: "Get expert guidance on IT strategies, system architecture, and digital transformation to optimize your business processes and enhance efficiency.",
+    icon: Service2,
+    delay: "400ms",
+  },
+  {
+    id: 3,
+    title: "UI/UX Design",
+    description: "Craft visually stunning and user-friendly interfaces that enhance user experience, engagement, and accessibility across all digital platforms.",
+    icon: Service3,
+    delay: "600ms",
+  },
+  {
+    id: 4,
+    title: "Web Development",
+    description: "Develop fast, responsive, and SEO-friendly websites using cutting-edge technologies to provide an engaging online experience for your users.",
+    icon: Service4,
+    delay: "800ms",
+  },
+  {
+    id: 5,
+    title: "Mobile App Development",
+    description: "Create intuitive, high-performance mobile applications for iOS and Android, ensuring seamless user experiences and business growth.",
+    icon: Service5,
+    delay: "400ms",
+  },
+  {
+    id: 6,
+    title: "Digital Marketing",
+    description: "Boost your brand’s online presence with data-driven digital marketing strategies, including SEO, social media marketing, PPC, and content marketing.",
+    icon: Service6,
+    delay: "200ms",
+  }
+];
